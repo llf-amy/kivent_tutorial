4d65e9400d752542ff8bb8f51843c1b74cdd2201
Amy Wooding <amy@learnleapfly.org>
2016-06-03 12:57:05 -0700

    Continuing through the tutorial. Lunch time.

--- main.py
+++ main.py
@@ -3,11 +3,29 @@ from kivy.uix.widget import Widget
 import kivent_core ## add this import statement to kivent
 
 class TestGame(Widget):
-    pass
+    def __init__(self, **kwargs):
+        super(TestGame, self).__init__(**kwargs)
+        ## add the position system to the gameworld initialization
+        self.gameworld.init_gameworld(['position'], callback = self.init_game)
+
+    def init_game(self):
+        self.setup_states()
+        self.set_state()
+
+    def setup_states(self):
+        self.gameworld.add_state(state_name='main',
+                                 systems_added = [],
+                                 systems_removed = [],
+                                 systems_paused = [],
+                                 systems_unpaused = [],
+                                 screenmanager_screen = 'main')
+    def set_state(self):
+        self.gameworld.state = 'main'
+
 
 class YourAppNameApp(App):
     def build(self):
         pass
-    
+
 if __name__ == '__main__':
     YourAppNameApp().run()
